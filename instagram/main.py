4d5134de08301750fac9dfdf061a93e6f7432eac
Grigoriy <grigoriy.program@gmail.com>
2022-04-21 00:32:55 +0300

    move urls to router /instagram

--- instagram/main.py
+++ instagram/main.py
@@ -1,14 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
 from .logic import InstagramMediaDownloader
 from .scheme import Media, MediaResponse, User, Settings, MediaType
 
-
 settings = Settings()
 app = FastAPI()
+instagram_router = APIRouter()
+app.openapi_version
 
 
-@app.get("/get-media-info", response_model=MediaResponse)
+@instagram_router.get("/get-media-info", response_model=MediaResponse)
 async def info(media_type: MediaType, media_id: str) -> MediaResponse:
     media = Media.from_orm(HANDLER_MEDIA[media_type](media_id.strip('/')))
     user = User.from_orm(media_downloader.get_user(media.owner_username))
@@ -21,3 +22,5 @@ HANDLER_MEDIA = {
     MediaType.REELS: media_downloader.get_reels,
     MediaType.STORIES: lambda media_id: media_downloader.get_stories(int(media_id)),
 }
+
+app.include_router(router=instagram_router, prefix='/instagram')
